62c8f43fe3010f86dede13c0df8c797b01622daa
phax <philip@helger.com>
2015-05-08 11:36:10 +0200

    Changed package names

---
+++ ph-oton-demo/src/main/java/com/helger/photon/bootstrap/demo/servlet/AppWebAppListener.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2014-2015 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.photon.bootstrap.demo.servlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.servlet.ServletContext;
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import com.helger.commons.annotations.Nonempty;
+import com.helger.photon.bootstrap.demo.app.AppSettings;
+import com.helger.photon.bootstrap.demo.app.init.AppSecurity;
+import com.helger.photon.bootstrap.demo.app.init.InitializerPublic;
+import com.helger.photon.bootstrap.demo.app.init.InitializerSecure;
+import com.helger.photon.bootstrap.demo.app.ui.AppCommonUI;
+import com.helger.photon.bootstrap3.servlet.AbstractWebAppListenerMultiAppBootstrap;
+import com.helger.photon.core.app.CApplication;
+import com.helger.photon.core.app.context.LayoutExecutionContext;
+import com.helger.photon.core.app.init.IApplicationInitializer;
+
+/**
+ * This listener is invoked during the servlet initiailization. This is
+ * basically a ServletContextListener.
+ *
+ * @author Philip Helger
+ */
+public final class AppWebAppListener extends AbstractWebAppListenerMultiAppBootstrap <LayoutExecutionContext>
+{
+  @Override
+  protected String getInitParameterDebug (@Nonnull final ServletContext aSC)
+  {
+    return AppSettings.getGlobalDebug ();
+  }
+
+  @Override
+  protected String getInitParameterProduction (@Nonnull final ServletContext aSC)
+  {
+    return AppSettings.getGlobalProduction ();
+  }
+
+  @Override
+  protected String getDataPath (@Nonnull final ServletContext aSC)
+  {
+    return AppSettings.getDataPath ();
+  }
+
+  @Override
+  protected boolean shouldCheckFileAccess (@Nonnull final ServletContext aSC)
+  {
+    return AppSettings.isCheckFileAccess ();
+  }
+
+  @Override
+  @Nonnull
+  @Nonempty
+  protected Map <String, IApplicationInitializer <LayoutExecutionContext>> getAllInitializers ()
+  {
+    final Map <String, IApplicationInitializer <LayoutExecutionContext>> ret = new HashMap <String, IApplicationInitializer <LayoutExecutionContext>> ();
+    ret.put (CApplication.APP_ID_SECURE, new InitializerSecure ());
+    ret.put (CApplication.APP_ID_PUBLIC, new InitializerPublic ());
+    return ret;
+  }
+
+  @Override
+  protected void initGlobals ()
+  {
+    // Internal stuff:
+
+    // JUL to SLF4J
+    SLF4JBridgeHandler.removeHandlersForRootLogger ();
+    SLF4JBridgeHandler.install ();
+
+    super.initGlobals ();
+
+    // UI stuff
+    AppCommonUI.init ();
+
+    // Set all security related stuff
+    AppSecurity.init ();
+  }
+}
