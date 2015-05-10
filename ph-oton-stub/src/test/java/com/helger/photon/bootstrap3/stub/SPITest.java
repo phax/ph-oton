d8c0cc2f67477748e094ba91cdac1cf2e05e70fc
phax <philip@helger.com>
2015-05-10 22:49:00 +0200

    Renamed project and packages

--- ph-oton-stub/src/test/java/com/helger/photon/bootstrap3/stub/SPITest.java
+++ ph-oton-stub/src/test/java/com/helger/photon/bootstrap3/stub/SPITest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.photon.stub;
+package com.helger.photon.bootstrap3.stub;
 
 import org.junit.Test;
 
