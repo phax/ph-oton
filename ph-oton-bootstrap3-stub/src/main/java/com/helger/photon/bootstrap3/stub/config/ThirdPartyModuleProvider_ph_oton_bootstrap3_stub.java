9b105e0d14205178879443c926bfbc8c39fed4db
phax <philip@helger.com>
2015-05-11 11:47:11 +0200

    Added Log4J2

---
+++ ph-oton-bootstrap3-stub/src/main/java/com/helger/photon/bootstrap3/stub/config/ThirdPartyModuleProvider_ph_oton_bootstrap3_stub.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2014-2015 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.photon.bootstrap3.stub.config;
+
+import javax.annotation.Nullable;
+
+import com.helger.commons.annotations.IsSPIImplementation;
+import com.helger.commons.thirdparty.ELicense;
+import com.helger.commons.thirdparty.IThirdPartyModule;
+import com.helger.commons.thirdparty.IThirdPartyModuleProviderSPI;
+import com.helger.commons.thirdparty.ThirdPartyModule;
+import com.helger.commons.version.Version;
+
+/**
+ * Implement this SPI interface if your JAR file contains external third party
+ * modules.
+ *
+ * @author Philip Helger
+ */
+@IsSPIImplementation
+public final class ThirdPartyModuleProvider_ph_oton_bootstrap3_stub implements IThirdPartyModuleProviderSPI
+{
+  public static final IThirdPartyModule LOG4J2 = new ThirdPartyModule ("Apache Log4j 2",
+                                                                       "Apache",
+                                                                       ELicense.APACHE2,
+                                                                       new Version (2, 2, 0),
+                                                                       "http://logging.apache.org/log4j/2.x/");
+
+  @Nullable
+  public IThirdPartyModule [] getAllThirdPartyModules ()
+  {
+    return new IThirdPartyModule [] { LOG4J2 };
+  }
+}
