f85fc49996df5e4874b33a19699f702ddecb85b7
phax <philip@helger.com>
2015-08-25 17:15:49 +0200

    Initial version for Bootstrap4

---
+++ ph-oton-bootstrap4/src/main/java/com/helger/photon/bootstrap4/EBootstrapJSPathProvider.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2014-2015 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.photon.bootstrap4;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.html.resource.js.ConstantJSPathProvider;
+import com.helger.html.resource.js.IJSPathProvider;
+
+public enum EBootstrapJSPathProvider implements IJSPathProvider
+{
+ /** Default complete Bootstrap JS */
+  BOOTSTRAP_400 ("bootstrap/4.0.0-dev/js/bootstrap.js");
+
+  private final ConstantJSPathProvider m_aPP;
+
+  private EBootstrapJSPathProvider (@Nonnull @Nonempty final String sPath)
+  {
+    m_aPP = ConstantJSPathProvider.create (sPath);
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getJSItemPath (final boolean bRegular)
+  {
+    return m_aPP.getJSItemPath (bRegular);
+  }
+
+  @Nullable
+  public String getConditionalComment ()
+  {
+    return m_aPP.getConditionalComment ();
+  }
+
+  public boolean isBundlable ()
+  {
+    return m_aPP.isBundlable ();
+  }
+}
---
+++ ph-oton-bootstrap4/src/test/java/com/helger/photon/bootstrap4/supplementary/tools/MainExtractBootstrap4CSSClasses.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2014-2015 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.photon.bootstrap4.supplementary.tools;
+
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.Nonnull;
+
+import com.helger.commons.charset.CCharset;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.css.ECSSVersion;
+import com.helger.css.decl.CSSSelector;
+import com.helger.css.decl.CSSSelectorSimpleMember;
+import com.helger.css.decl.CascadingStyleSheet;
+import com.helger.css.decl.ICSSSelectorMember;
+import com.helger.css.decl.visit.CSSVisitor;
+import com.helger.css.decl.visit.DefaultCSSVisitor;
+import com.helger.css.reader.CSSReader;
+import com.helger.photon.bootstrap4.EBootstrapCSSPathProvider;
+
+public class MainExtractBootstrap4CSSClasses
+{
+  public static void main (final String [] args)
+  {
+    final CascadingStyleSheet aCSS = CSSReader.readFromStream (new ClassPathResource (EBootstrapCSSPathProvider.BOOTSTRAP_400.getCSSItemPath (true)),
+                                                               CCharset.CHARSET_UTF_8_OBJ,
+                                                               ECSSVersion.CSS30);
+    final Set <String> aClasses = new TreeSet <String> ();
+    CSSVisitor.visitCSS (aCSS, new DefaultCSSVisitor ()
+    {
+      @Override
+      public void onStyleRuleSelector (@Nonnull final CSSSelector aSelector)
+      {
+        for (final ICSSSelectorMember aMember : aSelector.getAllMembers ())
+          if (aMember instanceof CSSSelectorSimpleMember)
+          {
+            final CSSSelectorSimpleMember aSM = (CSSSelectorSimpleMember) aMember;
+            if (aSM.isClass ())
+              aClasses.add (aSM.getValue ());
+            else
+              if (aSM.getValue ().contains ("."))
+                System.out.println (aSM.getValue ());
+          }
+      }
+    });
+    for (final String sClass : aClasses)
+    {
+      final String sClassName = sClass.substring (1);
+      String sFieldName = sClassName.toUpperCase (Locale.US);
+      sFieldName = sFieldName.replace ('-', '_');
+      System.out.println ("public static final ICSSClassProvider " +
+                          sFieldName +
+                          " = DefaultCSSClassProvider.create (\"" +
+                          sClassName +
+                          "\");");
+    }
+
+    System.out.println ();
+
+    // Icons
+    for (final String sClass : aClasses)
+      if (sClass.startsWith (".glyphicon-"))
+      {
+        final String sClassName = sClass.substring (1);
+        String sFieldName = sClassName.toUpperCase (Locale.US);
+        sFieldName = sFieldName.replace ('-', '_');
+        System.out.println (sFieldName.substring ("glyphicon-".length ()) + " (CBootstrapCSS." + sFieldName + "),");
+      }
+  }
+}
