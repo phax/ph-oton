994e37e8099801f0471398495cc967c70d4339d5
phax <philip@helger.com>
2016-01-12 22:42:08 +0100

    Changed to Predicate

--- ph-oton-basic/src/main/java/com/helger/photon/basic/app/dao/impl/IMapBasedDAO.java
+++ ph-oton-basic/src/main/java/com/helger/photon/basic/app/dao/impl/IMapBasedDAO.java
@@ -12,13 +12,13 @@
 package com.helger.photon.basic.app.dao.impl;
 
 import java.util.Collection;
+import java.util.function.Predicate;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
 import com.helger.commons.annotation.ReturnsMutableCopy;
-import com.helger.commons.filter.IFilter;
 import com.helger.commons.id.IHasID;
 
 @ThreadSafe
@@ -30,12 +30,12 @@ public interface IMapBasedDAO <INTERFACETYPE extends IHasID <String>>
 
   @Nonnull
   @ReturnsMutableCopy
-  Collection <? extends INTERFACETYPE> getAll (@Nullable IFilter <INTERFACETYPE> aFilter);
+  Collection <? extends INTERFACETYPE> getAll (@Nullable Predicate <INTERFACETYPE> aFilter);
 
   @Nullable
-  INTERFACETYPE getFirst (@Nullable IFilter <INTERFACETYPE> aFilter);
+  INTERFACETYPE getFirst (@Nullable Predicate <INTERFACETYPE> aFilter);
 
-  boolean containsAny (@Nullable IFilter <INTERFACETYPE> aFilter);
+  boolean containsAny (@Nullable Predicate <INTERFACETYPE> aFilter);
 
   boolean containsWithID (@Nullable String sID);
 }
