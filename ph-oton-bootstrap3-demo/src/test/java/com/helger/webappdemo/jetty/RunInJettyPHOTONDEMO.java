752a67264f2fe269fd6bddce89405957c166388f
phax <philip@helger.com>
2016-01-26 20:35:45 +0100

    Improved Jetty startup code

--- ph-oton-bootstrap3-demo/src/test/java/com/helger/webappdemo/jetty/RunInJettyPHOTONDEMO.java
+++ ph-oton-bootstrap3-demo/src/test/java/com/helger/webappdemo/jetty/RunInJettyPHOTONDEMO.java
@@ -103,9 +103,10 @@ public final class RunInJettyPHOTONDEMO
       // Starting the engines:
       aServer.start ();
     }
-    catch (final Exception ex)
+    catch (final Throwable t)
     {
-      throw new IllegalStateException ("Failed to start server!", ex);
+      // Do not throw something here, in case some exception occurs in stop code
+      s_aLogger.error ("Failed to start server!", t);
     }
     finally
     {
