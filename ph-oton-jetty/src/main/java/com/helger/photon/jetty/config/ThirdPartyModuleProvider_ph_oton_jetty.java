d76b387a00fe5712e3e2c9efc463c8c3950450bc
Philip Helger <philip@helger.com>
2016-06-30 13:52:24 +0200

    Updated to Jetty 9.3.10.v20160621

--- ph-oton-jetty/src/main/java/com/helger/photon/jetty/config/ThirdPartyModuleProvider_ph_oton_jetty.java
+++ ph-oton-jetty/src/main/java/com/helger/photon/jetty/config/ThirdPartyModuleProvider_ph_oton_jetty.java
@@ -37,7 +37,7 @@ public final class ThirdPartyModuleProvider_ph_oton_jetty implements IThirdParty
   public static final IThirdPartyModule JETTY = new ThirdPartyModule ("Jetty",
                                                                       "Eclipse",
                                                                       ELicense.EPL10,
-                                                                      new Version (9, 3, 7),
+                                                                      new Version (9, 3, 10),
                                                                       "https://eclipse.org/jetty/");
 
   @Nullable
