859191049ef96fac9c2566ebb0d30d922fef040e
Philip Helger <philip@helger.com>
2017-08-11 11:41:34 +0200

    Better test handlign

--- ph-oton-basic/src/main/java/com/helger/photon/basic/app/dao/AbstractPhotonSimpleDAO.java
+++ ph-oton-basic/src/main/java/com/helger/photon/basic/app/dao/AbstractPhotonSimpleDAO.java
@@ -4,6 +4,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.helger.commons.functional.ISupplier;
+import com.helger.commons.io.relative.FileRelativeIO;
 import com.helger.dao.simple.AbstractSimpleDAO;
 import com.helger.photon.basic.app.io.WebFileIO;
 
@@ -11,7 +12,9 @@ public abstract class AbstractPhotonSimpleDAO extends AbstractSimpleDAO
 {
   protected AbstractPhotonSimpleDAO (@Nullable final String sFilename)
   {
-    this ( () -> sFilename);
+    // Kind of a test fix, because when no filename is present, WebFilIO also
+    // must not be inited. Any DAO IO is fine
+    super (sFilename != null ? WebFileIO.getDataIO () : FileRelativeIO.createForCurrentDir (), () -> sFilename);
   }
 
   protected AbstractPhotonSimpleDAO (@Nonnull final ISupplier <String> aFilenameProvider)
