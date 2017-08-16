7b0c34ca1255a624c3af9d21e2bfd27ee94c2e1e
Philip Helger <philip@helger.com>
2017-08-16 10:36:10 +0200

    Switched to XServlet

---
+++ ph-oton-core/src/main/java/com/helger/photon/core/servlet/CSPReportingXServletHandler.java
@@ -0,0 +1,65 @@
+package com.helger.photon.core.servlet;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+import javax.annotation.Nonnull;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.io.stream.NonBlockingByteArrayInputStream;
+import com.helger.commons.io.stream.StreamHelper;
+import com.helger.http.EHttpMethod;
+import com.helger.http.EHttpVersion;
+import com.helger.json.IJson;
+import com.helger.json.serialize.JsonReader;
+import com.helger.json.serialize.JsonWriterSettings;
+import com.helger.web.scope.IRequestWebScope;
+import com.helger.xservlet.handler.IXServletHandler;
+
+/**
+ * Default Servlet handler for CSP reporting. Used in
+ * {@link DefaultCSPReportingServlet}.
+ * 
+ * @author Philip Helger
+ */
+public class CSPReportingXServletHandler implements IXServletHandler
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (CSPReportingXServletHandler.class);
+
+  private final Consumer <? super IJson> m_aJsonHandler;
+
+  public CSPReportingXServletHandler ()
+  {
+    this (aJson -> s_aLogger.warn ("CSP report: " +
+                                   aJson.getAsJsonString (new JsonWriterSettings ().setIndentEnabled (true))));
+  }
+
+  public CSPReportingXServletHandler (final Consumer <? super IJson> aJsonHandler)
+  {
+    m_aJsonHandler = ValueEnforcer.notNull (aJsonHandler, "JsonHandler");
+  }
+
+  public void onRequest (@Nonnull final HttpServletRequest aHttpRequest,
+                         @Nonnull final HttpServletResponse aHttpResponse,
+                         @Nonnull final EHttpVersion eHttpVersion,
+                         @Nonnull final EHttpMethod eHttpMethod,
+                         @Nonnull final IRequestWebScope aRequestScope) throws ServletException, IOException
+  {
+    // Read all request body bytes
+    final byte [] aBytes = StreamHelper.getAllBytes (aHttpRequest.getInputStream ());
+
+    // Try to parse as JSON
+    final IJson aJson = JsonReader.readFromStream (new NonBlockingByteArrayInputStream (aBytes));
+    if (aJson != null)
+      m_aJsonHandler.accept (aJson);
+    else
+      s_aLogger.error ("Failed to parse CSP report JSON: " + new String (aBytes, StandardCharsets.ISO_8859_1));
+  }
+}
--- ph-oton-core/src/main/java/com/helger/photon/core/servlet/DefaultCSPReportingServlet.java
+++ ph-oton-core/src/main/java/com/helger/photon/core/servlet/DefaultCSPReportingServlet.java
@@ -16,25 +16,8 @@
  */
 package com.helger.photon.core.servlet;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-import javax.annotation.Nonnull;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.helger.commons.annotation.OverrideOnDemand;
-import com.helger.commons.io.stream.NonBlockingByteArrayInputStream;
-import com.helger.commons.io.stream.StreamHelper;
-import com.helger.json.IJson;
-import com.helger.json.serialize.JsonReader;
-import com.helger.json.serialize.JsonWriterSettings;
-import com.helger.xservlet.servletstatus.ServletStatusManager;
+import com.helger.http.EHttpMethod;
+import com.helger.xservlet.AbstractXServlet;
 
 /**
  * A simple default implementation of a CSP (Content Security Policy) reporting
@@ -42,61 +25,13 @@ import com.helger.xservlet.servletstatus.ServletStatusManager;
  *
  * @author Philip Helger
  */
-public class DefaultCSPReportingServlet extends HttpServlet
+public class DefaultCSPReportingServlet extends AbstractXServlet
 {
   public static final String SERVLET_DEFAULT_NAME = "cspreporting";
   public static final String SERVLET_DEFAULT_PATH = '/' + SERVLET_DEFAULT_NAME;
 
-  private static final Logger s_aLogger = LoggerFactory.getLogger (DefaultCSPReportingServlet.class);
-
-  private final ServletStatusManager m_aStatusMgr;
-
   public DefaultCSPReportingServlet ()
   {
-    // Remember to avoid crash on shutdown, when no GlobalScope is present
-    m_aStatusMgr = ServletStatusManager.getInstance ();
-    m_aStatusMgr.onServletCtor (getClass ());
-  }
-
-  @Override
-  public void init () throws ServletException
-  {
-    m_aStatusMgr.onServletInit (getClass ());
-  }
-
-  /**
-   * Callback method that is invoked for correctly parsed CSP report JSON
-   * object.
-   *
-   * @param aJson
-   *        The parsed JSON. Never <code>null</code>.
-   */
-  @OverrideOnDemand
-  protected void handleCSPReport (@Nonnull final IJson aJson)
-  {
-    s_aLogger.warn ("CSP report: " + aJson.getAsJsonString (new JsonWriterSettings ().setIndentEnabled (true)));
-  }
-
-  @Override
-  protected void doPost (@Nonnull final HttpServletRequest aHttpRequest,
-                         @Nonnull final HttpServletResponse aHttpResponse) throws ServletException, IOException
-  {
-    m_aStatusMgr.onServletInvocation (getClass ());
-
-    // Read all request body bytes
-    final byte [] aBytes = StreamHelper.getAllBytes (aHttpRequest.getInputStream ());
-
-    // Try to parse as JSON
-    final IJson aJson = JsonReader.readFromStream (new NonBlockingByteArrayInputStream (aBytes));
-    if (aJson != null)
-      handleCSPReport (aJson);
-    else
-      s_aLogger.error ("Failed to parse CSP report JSON: " + new String (aBytes, StandardCharsets.ISO_8859_1));
-  }
-
-  @Override
-  public void destroy ()
-  {
-    m_aStatusMgr.onServletDestroy (getClass ());
+    handlerRegistry ().registerHandler (EHttpMethod.POST, new CSPReportingXServletHandler ());
   }
 }
