b0d621453cd895362dbc73eb2cd21c1db65cc91f
Philip Helger <philip@helger.com>
2017-09-07 20:31:30 +0200

    Updated license headers

--- ph-oton-basic/src/main/java/com/helger/photon/xservlet/RootXServletHandler.java
+++ ph-oton-basic/src/main/java/com/helger/photon/xservlet/RootXServletHandler.java
@@ -1,18 +1,26 @@
 /**
- * Copyright (C) 2006-2017 BRZ GmbH
- * http://www.brz.gv.at
+ * Copyright (C) 2014-2017 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
  *
- * All rights reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.helger.photon.xservlet;
 
 import javax.annotation.Nonnull;
 
-import com.helger.commons.ValueEnforcer;
-import com.helger.commons.string.StringHelper;
-import com.helger.servlet.response.UnifiedResponse;
-import com.helger.web.scope.IRequestWebScopeWithoutResponse;
+import com.helger.commons.annotation.Nonempty;
 import com.helger.xservlet.handler.simple.IXServletSimpleHandler;
+import com.helger.xservlet.handler.specific.RedirectToServletXServletHandler;
 
 /**
  * An {@link IXServletSimpleHandler} that does the necessary redirect for a ROOT
@@ -21,27 +29,10 @@ import com.helger.xservlet.handler.simple.IXServletSimpleHandler;
  *
  * @author Philip Helger
  */
-public class RootXServletHandler implements IXServletSimpleHandler
+public class RootXServletHandler extends RedirectToServletXServletHandler
 {
-  private final String m_sServletPath;
-
-  public RootXServletHandler (final String sServletPath)
+  public RootXServletHandler (@Nonnull @Nonempty final String sServletPath)
   {
-    ValueEnforcer.notEmpty (sServletPath, "ServletPath");
-    ValueEnforcer.isTrue (sServletPath.startsWith ("/"), "Path must start with '/'!");
-
-    m_sServletPath = sServletPath;
-  }
-
-  public void handleRequest (@Nonnull final IRequestWebScopeWithoutResponse aRequestScope,
-                             @Nonnull final UnifiedResponse aUnifiedResponse) throws Exception
-  {
-    String sRedirectURL = aRequestScope.getContextPath () + m_sServletPath;
-
-    final String sQueryString = aRequestScope.getQueryString ();
-    if (StringHelper.hasText (sQueryString))
-      sRedirectURL += "?" + sQueryString;
-
-    aUnifiedResponse.setRedirect (sRedirectURL);
+    super (sServletPath);
   }
 }
