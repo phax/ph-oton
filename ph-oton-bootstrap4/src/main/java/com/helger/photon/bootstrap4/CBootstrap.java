a01b732a38b7d5d3f8dbbb00d53c16322b63c37b
Philip Helger <philip@helger.com>
2018-07-20 08:39:32 +0200

    Updated to Bootstrap 4.1.2

--- ph-oton-bootstrap4/src/main/java/com/helger/photon/bootstrap4/CBootstrap.java
+++ ph-oton-bootstrap4/src/main/java/com/helger/photon/bootstrap4/CBootstrap.java
@@ -29,7 +29,7 @@ import com.helger.commons.version.Version;
 public final class CBootstrap
 {
   /** Bootstrap version */
-  public static final Version BOOTSTRAP_VERSION_41 = new Version (4, 1, 1);
+  public static final Version BOOTSTRAP_VERSION_41 = new Version (4, 1, 2);
 
   /** The maximum number of columns a grid system can be separated into */
   public static final int GRID_SYSTEM_MAX = 12;
