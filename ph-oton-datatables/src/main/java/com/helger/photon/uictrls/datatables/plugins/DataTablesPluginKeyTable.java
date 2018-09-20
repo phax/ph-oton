01605ddc1c8a9bb3a6a639602d75556fc5a0baa3
Philip Helger <philip@helger.com>
2018-09-21 00:18:26 +0200

    new plugin stubs

---
+++ ph-oton-datatables/src/main/java/com/helger/photon/uictrls/datatables/plugins/DataTablesPluginKeyTable.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2014-2018 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.photon.uictrls.datatables.plugins;
+
+import javax.annotation.Nullable;
+
+import com.helger.html.hc.IHCConversionSettingsToNode;
+import com.helger.html.jscode.IJSExpression;
+import com.helger.html.jscode.JSAssocArray;
+import com.helger.html.jscode.JSExpr;
+import com.helger.photon.core.app.html.PhotonCSS;
+import com.helger.photon.core.app.html.PhotonJS;
+import com.helger.photon.uictrls.datatables.EDataTablesCSSPathProvider;
+import com.helger.photon.uictrls.datatables.EDataTablesJSPathProvider;
+
+public class DataTablesPluginKeyTable extends AbstractDataTablesPlugin
+{
+  public DataTablesPluginKeyTable ()
+  {
+    super ("KeyTable");
+  }
+
+  @Nullable
+  public IJSExpression getInitParams ()
+  {
+    final JSAssocArray ret = new JSAssocArray ();
+    if (ret.isEmpty ())
+    {
+      // No properties present
+      return JSExpr.TRUE;
+    }
+    return ret;
+  }
+
+  @Override
+  public void registerExternalResources (final IHCConversionSettingsToNode aConversionSettings)
+  {
+    PhotonJS.registerJSIncludeForThisRequest (EDataTablesJSPathProvider.DATATABLES_KEY_TABLE);
+    PhotonCSS.registerCSSIncludeForThisRequest (EDataTablesCSSPathProvider.DATATABLES_KEY_TABLE);
+  }
+}
