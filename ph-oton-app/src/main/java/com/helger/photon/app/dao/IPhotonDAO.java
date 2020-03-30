e488a63ee2559be02c418a826d1be4c7e0297d5c
Philip Helger <philip@helger.com>
2020-03-30 16:46:34 +0200

    License headers

--- ph-oton-app/src/main/java/com/helger/photon/app/dao/IPhotonDAO.java
+++ ph-oton-app/src/main/java/com/helger/photon/app/dao/IPhotonDAO.java
@@ -1,6 +1,23 @@
+/**
+ * Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.photon.app.dao;
 
 import java.io.Serializable;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import javax.annotation.Nonnegative;
@@ -38,6 +55,10 @@ public interface IPhotonDAO <INTERFACETYPE extends IHasID <String> & Serializabl
   @ReturnsMutableCopy
   ICommonsList <INTERFACETYPE> getAll ();
 
+  @Nonnull
+  @ReturnsMutableCopy
+  <T> ICommonsList <T> getNone ();
+
   /**
    * @param aFilter
    *        The filter to be applied. May not be <code>null</code>.
@@ -47,6 +68,9 @@ public interface IPhotonDAO <INTERFACETYPE extends IHasID <String> & Serializabl
   @Nonnull
   @ReturnsMutableCopy
   ICommonsList <INTERFACETYPE> getAll (@Nullable Predicate <? super INTERFACETYPE> aFilter);
+
+  <RETTYPE> ICommonsList <RETTYPE> getAllMapped (@Nullable Predicate <? super INTERFACETYPE> aFilter,
+                                                 @Nonnull Function <? super INTERFACETYPE, ? extends RETTYPE> aMapper);
 
   @Nonnegative
   int getCount (@Nullable Predicate <? super INTERFACETYPE> aFilter);
