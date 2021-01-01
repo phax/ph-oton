dc09817072c09f9efa27889152409a9425a75f13
Philip Helger <philip@helger.com>
2021-01-01 13:50:15 +0100

    Updated to 2021

--- ph-oton-uictrls/src/test/resources/fineupload/320/merge.cmd
+++ ph-oton-uictrls/src/test/resources/fineupload/320/merge.cmd
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+@REM Copyright (C) 2014-2021 Philip Helger (www.helger.com)
 @REM philip[at]helger[dot]com
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
